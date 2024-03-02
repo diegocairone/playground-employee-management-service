cccd4db03a09dbe000850e97838ae734618d7488
Diego Cairone <diegocairone@gmail.com>
2024-03-01 21:34:35 -0300

    Renaming project (#11)
    * renaming
    * fix errors in test db

--- employees-manager-service/src/test/resources/data.sql
+++ employees-manager-service/src/test/resources/data.sql
@@ -31,23 +31,27 @@ INSERT INTO banks_accounts (account_id, account_number, account_type, bank_id) V
 (1, 'SS00000001', 1, 1),
 (2, 'SS00000002', 1, 1),
 (3, 'OO00000003', 0, 2),
-(4, 'OO00000004', 0, 2)
+(4, 'OO00000004', 0, 2),
+(5, 'OO00000005', 0, 2),
+(6, 'OO00000006', 0, 2),
+(7, 'OO00000007', 0, 2),
+(8, 'OO00000008', 0, 2)
 ;
 
 ALTER SEQUENCE bank_account_seq RESTART WITH 10;
 
 
 INSERT INTO employees
-(employee_id, names, birthdate, city_id, status, bank_account_id)
+(employee_id, global_id, names, birthdate, city_id, status, bank_account_id)
 VALUES
-(1, 'EMPLOYEE-1', '1980-02-03', 1, 1, 1),
-(2, 'EMPLOYEE-2', '1980-02-03', 1, 1, 2),
-(3, 'EMPLOYEE-3', '1980-02-03', 1, 1, 3),
-(4, 'HOMER SIMPSON', '1980-02-03', 3, 1, null),
-(5, 'MARGE SIMPSON', '1980-02-03', 3, 1, null),
-(6, 'BART SIMPSON', '1990-02-03', 3, 1, null),
-(7, 'LISA SIMPSON', '1995-02-03', 3, 1, null),
-(8, 'MAGGIE SIMPSON', '2000-02-03', 3, 1, null)
+(1, 'e71259c0-7438-4752-be3a-1a664de706e8', 'EMPLOYEE-1', '1980-02-03', 1, 1, 1),
+(2, '81298f47-0af9-4604-ade0-8ecc6d151921', 'EMPLOYEE-2', '1980-02-03', 1, 1, 2),
+(3, '75f60d67-587e-46bc-b223-9c48356114fc', 'EMPLOYEE-3', '1980-02-03', 1, 1, 3),
+(4, '7ccc6f9a-b4ac-4c54-85d2-59fd4ccfe221', 'HOMER SIMPSON', '1980-02-03', 3, 1, 4),
+(5, '1ed1d31f-3cad-4873-aabc-45ceb8d8ffee', 'MARGE SIMPSON', '1980-02-03', 3, 1, 5),
+(6, '5a61f181-b88c-4974-a6a3-3416ca337706', 'BART SIMPSON', '1990-02-03', 3, 1, 6),
+(7, '95a0066a-ce58-4eae-9f7a-6cf2dcd85c1a', 'LISA SIMPSON', '1995-02-03', 3, 1, 7),
+(8, '2c2ea326-751e-4870-93a5-d25fd29c5ec1', 'MAGGIE SIMPSON', '2000-02-03', 3, 1, 8)
 ;
 
 ALTER SEQUENCE employee_seq RESTART WITH 10;
